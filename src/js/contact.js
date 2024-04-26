a6e7ab37da9764c7d0745afae89b4b9d52ac550c
ComplexRalex <ralexbatres@gmail.com>
2024-04-25 22:25:53 -0600

    Changed bio, rearranged content cards, and tweaked other stuff

--- src/js/contact.js
+++ src/js/contact.js
@@ -11,8 +11,8 @@ const createContact = () => {
     element.innerHTML = `
       <p class="fs-2 section-title text-shadow">${info["title"].replaceAll('\n','<br>')}</p>
       <div class="text-white bg-dark div-content text-center box-shadow">
-          <p class="fs-5 text-break">Email: <a href="mailto:${me.email}">${me.email}</a></p>
-          <div class="mt-4">
+          <p class="fs-5 text-break"><a href="mailto:${me.email}">${me.email}</a></p>
+          <div class="mt-4 mb-1">
             ${mapIcons(me.social,'circle-sn-m','rounded-3')}
           </div>
       </div>
